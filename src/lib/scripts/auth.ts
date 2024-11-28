de3fed82a588d5b58f55c8c21288e1b5c066a357
garryofc <havelkastepan5@gmail.com>
2024-11-28 12:59:38 +0100

    scripts

---
+++ src/lib/scripts/auth.ts
@@ -0,0 +1,17 @@
+export class Auth {
+	static login() {
+		console.log('login');
+	}
+
+	static register() {
+		console.log('register');
+	}
+
+	static check() {
+		console.log('check');
+	}
+
+	static logout() {
+		console.log('logout');
+	}
+}
