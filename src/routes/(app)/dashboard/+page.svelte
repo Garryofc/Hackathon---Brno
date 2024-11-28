a3a4ed2edea2f40509622096ca710d6e51e4b7e8
Koninec <davidzdimera007@gmail.com>
2024-11-28 20:50:28 +0100

    1.5 desbort

--- src/routes/(app)/dashboard/+page.svelte
+++ src/routes/(app)/dashboard/+page.svelte
@@ -21,6 +21,7 @@
     import * as Sheet from "$lib/components/ui/sheet/index.js";
     import * as Table from "$lib/components/ui/table/index.js";
     import { onMount } from "svelte";
+	import { Description } from "formsnap";
 
 
 
@@ -108,69 +109,29 @@
           </Card.Content>
         </Card.Root>
 
-        <Card.Root class="col-span-5">0
+        <Card.Root class="col-span-5">
+          <Card.Header>
+            <Card.Title>neco</Card.Title>
+            <Card.Description>Neco</Card.Description>
+          </Card.Header>
+          
+          
         </Card.Root>
 
         <Card.Root class="col-span-4">
           <Card.Header>
             <Card.Title>Recent Sales</Card.Title>
+            <Card.Description>x sales this month</Card.Description>
           </Card.Header>
-          <Card.Content class="grid gap-8">
-            <div class="flex items-center gap-4">
-              <Avatar.Root class="hidden h-9 w-9 sm:flex">
-                <Avatar.Image src="/avatars/01.png" alt="Avatar" />
-                <Avatar.Fallback>OM</Avatar.Fallback>
-              </Avatar.Root>
-              <div class="grid gap-1">
-                <p class="text-sm font-medium leading-none">Olivia Martin</p>
-                <p class="text-muted-foreground text-sm">olivia.martin@email.com</p>
+          <Card.Content class="gap-8 flex flex-col">  
+            {#each Array(7) as _, i} 
+            <div class="flex items-center justify-between">
+              <div class="">
+                <p class="text-sm font-medium leading-none">Product {i+1}</p>
               </div>
-              <div class="ml-auto font-medium">+$1,999.00</div>
+              <div class="font-medium text-right">+$1,999.00</div>
             </div>
-            <div class="flex items-center gap-4">
-              <Avatar.Root class="hidden h-9 w-9 sm:flex">
-                <Avatar.Image src="/avatars/02.png" alt="Avatar" />
-                <Avatar.Fallback>JL</Avatar.Fallback>
-              </Avatar.Root>
-              <div class="grid gap-1">
-                <p class="text-sm font-medium leading-none">Jackson Lee</p>
-                <p class="text-muted-foreground text-sm">jackson.lee@email.com</p>
-              </div>
-              <div class="ml-auto font-medium">+$39.00</div>
-            </div>
-            <div class="flex items-center gap-4">
-              <Avatar.Root class="hidden h-9 w-9 sm:flex">
-                <Avatar.Image src="/avatars/03.png" alt="Avatar" />
-                <Avatar.Fallback>IN</Avatar.Fallback>
-              </Avatar.Root>
-              <div class="grid gap-1">
-                <p class="text-sm font-medium leading-none">Isabella Nguyen</p>
-                <p class="text-muted-foreground text-sm">isabella.nguyen@email.com</p>
-              </div>
-              <div class="ml-auto font-medium">+$299.00</div>
-            </div>
-            <div class="flex items-center gap-4">
-              <Avatar.Root class="hidden h-9 w-9 sm:flex">
-                <Avatar.Image src="/avatars/04.png" alt="Avatar" />
-                <Avatar.Fallback>WK</Avatar.Fallback>
-              </Avatar.Root>
-              <div class="grid gap-1">
-                <p class="text-sm font-medium leading-none">William Kim</p>
-                <p class="text-muted-foreground text-sm">will@email.com</p>
-              </div>
-              <div class="ml-auto font-medium">+$99.00</div>
-            </div>
-            <div class="flex items-center gap-4">
-              <Avatar.Root class="hidden h-9 w-9 sm:flex">
-                <Avatar.Image src="/avatars/05.png" alt="Avatar" />
-                <Avatar.Fallback>SD</Avatar.Fallback>
-              </Avatar.Root>
-              <div class="grid gap-1">
-                <p class="text-sm font-medium leading-none">Sofia Davis</p>
-                <p class="text-muted-foreground text-sm">sofia.davis@email.com</p>
-              </div>
-              <div class="ml-auto font-medium">+$39.00</div>
-            </div>
+            {/each}
           </Card.Content>
         </Card.Root>
       </div>
