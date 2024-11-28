97f8c74c3b493f444c4f889415d90aafc3c41a78
garryofc <havelkastepan5@gmail.com>
2024-11-28 14:22:02 +0100

    lines

--- src/lib/components/env/fbConnect.svelte
+++ src/lib/components/env/fbConnect.svelte
@@ -3,10 +3,19 @@
 </script>
 
 {#if $addFb}
-	<main class="fixed left-0 top-0 flex h-full w-full flex-col items-center justify-center bg-white">
-		<h1 class="mb-8 text-center text-4xl font-bold md:text-5xl">
+	<main
+		class="fixed left-0 top-0 flex h-full w-full flex-col items-center justify-center gap-12 bg-white"
+	>
+		<h1 class="text-center text-4xl font-bold md:text-5xl">
 			Add your
-			<span class="font-bold text-primary">Facebook</span>
+			<span class="relative inline-block text-primary">
+				Facebook
+				<img
+					class="absolute bottom-0 left-0 h-fit w-full translate-y-[53%]"
+					src="/underline.png"
+					alt="underline"
+				/>
+			</span>
 			account
 		</h1>
 		<div class="h-auto w-fit">
@@ -17,9 +26,16 @@
 					</div>
 				{/each}
 			{:else}
-				<button class="flex items-center gap-4 rounded-md bg-primary px-4 py-2.5 text-white">
+				<button
+					class="relative flex items-center gap-4 rounded-md bg-primary px-4 py-2.5 text-white"
+				>
 					<i class="bi bi-facebook text-2xl"></i>
 					<h3>Add Facebook account</h3>
+					<img
+						src="/arrowFTTL.png"
+						alt="arrow-right"
+						class="absolute right-0 top-0 translate-x-[130%] translate-y-[-40%]"
+					/>
 				</button>
 			{/if}
 		</div>
