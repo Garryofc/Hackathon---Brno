afbe94d66a97e0d7abfc296e8966903d91c04213
misrtjakub <pokorny2007@gmail.com>
2024-11-28 12:17:11 +0100

    fwiuefw

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -2,15 +2,6 @@
 	import '../../app.css';
   </script>
   
-  <style>
-	main {
-	  background-image: url('/landingBG.jpg');
-	  background-size: cover; 
-	  background-position: center; 
-	  min-height: 100vh; 
-	}
-  </style>
-  
   <main>
 	<slot />
   </main>
