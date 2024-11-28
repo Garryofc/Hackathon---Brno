e3acb36399cec556e24db26e787d8e7688bb6921
garryofc <havelkastepan5@gmail.com>
2024-11-28 11:47:29 +0100

    edit

---
+++ src/routes/api/auth/registration/+server.ts
@@ -0,0 +1,87 @@
+import { db } from '$lib/db.js';
+import { redirect } from '@sveltejs/kit';
+import bcrypt from 'bcrypt';
+
+function tokengen() {
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	let token = '';
+	for (let i = 0; i < 50; i++) {
+		const randomIndex = Math.floor(Math.random() * characters.length);
+		token += characters[randomIndex];
+	}
+	return token;
+}
+
+var email_regex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,4}$/;
+
+export async function POST({ request }) {
+	const body = await request.json();
+
+	body.email = body.email.replace(/\s/g, '');
+
+	if (!email_regex.test(body.email)) {
+		return new Response(
+			JSON.stringify({ message: 'Email does not match format xxx@xxx.xx', status: 401 }),
+			{
+				status: 401,
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			}
+		);
+	}
+
+	if (body.password.length < 8) {
+		return new Response(
+			JSON.stringify({ message: 'Password must be at least 8 characters long', status: 401 }),
+			{
+				status: 401,
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			}
+		);
+	}
+
+	const user = await db.user.findUnique({
+		where: { email: body.email }
+	});
+
+	if (!user) {
+		var userAuthToken = tokengen();
+
+		const passwordHash = await bcrypt.hash(body.password, 10);
+
+		await db.user.create({
+			data: {
+				email: body.email,
+				passwordHash: passwordHash,
+				userAuthToken: userAuthToken
+			}
+		});
+
+		return new Response(
+			JSON.stringify({
+				message: 'User created',
+				status: 200,
+				redirect: `/verify?email=${body.email}`
+			}),
+			{
+				status: 200,
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			}
+		);
+	} else {
+		return new Response(
+			JSON.stringify({ message: 'User with this email already exists', status: 401 }),
+			{
+				status: 401,
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			}
+		);
+	}
+}
